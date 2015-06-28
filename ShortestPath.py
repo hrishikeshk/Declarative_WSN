61c7e07fc62cdb5c77d87d7f9ccbc81d1d8a6e7e
Hrishikesh VK <kv_hrishikesh@yahoo.com>
2015-06-28 23:17:47 +0530

    Printed the Shortest path as well as Cost in the graph as per min function

--- ShortestPath.py
+++ ShortestPath.py
@@ -10,23 +10,11 @@ pyDatalog.create_terms("Y, Minimum, Src, Dest, Cost, Cost1, Cost2, Next, Path, P
 +link(b, d, 1)
 +link(c, b, 1)
 
-def Minimum(X):
-    return min_(X, order_by=Cost1) 
-
-
 #recursive rules to define paths with cost
 path(Src, Dest, Path, Cost) <= link(Src, Dest, Cost) & (Path == [Src] + [Dest])
 path(Src, Dest, Path, Cost) <= link(Src, Next, Cost1) & path(Next, Dest, Path2, Cost2) & (Src != Dest) & (Path == [Src] + Path2) & (Cost == Cost1 + Cost2)
 
-#spCost(Src, Dest, Cost) <= (Y == path(Src, Dest, Path, Cost1)) & ( Cost == Minimum(Cost1) )
-#spCost(Src, Dest, Cost) <= ( Cost == Minimum(Y) ) & (Y == path(Src, Dest, Path, Cost1))
-#spCost(Src, Dest, Cost) <= ( Cost == min(X, order_by=Cost1) ) & (X == path(Src, Dest, Path, Cost1))
-#spCost(Src, Dest, Cost) <= ( Cost == min(Cost1) ) & (X == path(Src, Dest, Path, Cost1))
+(shortestPath[Src, Dest]==min_( [Path, Cost] , order_by=Cost)) <= (path( Src , Dest, Path, Cost))
 
-#shortestPath(Src, Dest, Path, Cost) <= (Cost == spCost(Y)) & ( Y == path(Src, Dest, Path, Cost) )
-
-(shortestPath[Src, Dest]==min_(Path, order_by=Cost)) <= (path( Src , Dest, Path, Cost))
-
-#print (shortestPath(Src, Dest, Path, Cost)) & (Src == a) & (Dest == d)
-print (shortestPath [Src, Dest] == Path ) & (Src == a) & (Dest == d)
+print (shortestPath [Src, Dest] == [Path, Cost] ) & (Src == a) & (Dest == d)
 
