121c185149b6785dc288e470009e70ae0799da98
Hrishikesh VK <kv_hrishikesh@yahoo.com>
2015-06-28 23:11:10 +0530

    Printed the Shortest path in the graph as per min function

--- ShortestPath.py
+++ ShortestPath.py
@@ -1,7 +1,7 @@
 # This centralized (in other words, simulated) shortest path routing
 
 from pyDatalog import pyDatalog
-pyDatalog.create_terms("Src, Dest, Cost, Cost1, Cost2, Next, Path, Path2, path, link, edge, a, b, c, d, e")
+pyDatalog.create_terms("Y, Minimum, Src, Dest, Cost, Cost1, Cost2, Next, Path, Path2, path, link, a, b, c, d, e, spCost, shortestPath, X")
 
 #links with cost
 +link(e, a, 1)
@@ -10,9 +10,23 @@ pyDatalog.create_terms("Src, Dest, Cost, Cost1, Cost2, Next, Path, Path2, path, 
 +link(b, d, 1)
 +link(c, b, 1)
 
+def Minimum(X):
+    return min_(X, order_by=Cost1) 
+
+
 #recursive rules to define paths with cost
 path(Src, Dest, Path, Cost) <= link(Src, Dest, Cost) & (Path == [Src] + [Dest])
-path(Src, Dest, Path, Cost) <= link(Src, Next, Cost1) & path(Next, Dest, Path2, Cost2) & Path == [Src] + Path2 & Cost == Cost1 + Cost2
-spCost(Src, Dest, Cost) <= path(Src, Dest, Path, Cost1) & (Cost == min_(Cost1)) 
-shortestPath(Src, Dest, Path, Cost) <= spCost(Src, Dest, Cost) & path(Src, Dest, Path, Cost)
-print (shortestPath(Src, Dest, Path, Cost)) & (Src == a) & (Dest == d)
+path(Src, Dest, Path, Cost) <= link(Src, Next, Cost1) & path(Next, Dest, Path2, Cost2) & (Src != Dest) & (Path == [Src] + Path2) & (Cost == Cost1 + Cost2)
+
+#spCost(Src, Dest, Cost) <= (Y == path(Src, Dest, Path, Cost1)) & ( Cost == Minimum(Cost1) )
+#spCost(Src, Dest, Cost) <= ( Cost == Minimum(Y) ) & (Y == path(Src, Dest, Path, Cost1))
+#spCost(Src, Dest, Cost) <= ( Cost == min(X, order_by=Cost1) ) & (X == path(Src, Dest, Path, Cost1))
+#spCost(Src, Dest, Cost) <= ( Cost == min(Cost1) ) & (X == path(Src, Dest, Path, Cost1))
+
+#shortestPath(Src, Dest, Path, Cost) <= (Cost == spCost(Y)) & ( Y == path(Src, Dest, Path, Cost) )
+
+(shortestPath[Src, Dest]==min_(Path, order_by=Cost)) <= (path( Src , Dest, Path, Cost))
+
+#print (shortestPath(Src, Dest, Path, Cost)) & (Src == a) & (Dest == d)
+print (shortestPath [Src, Dest] == Path ) & (Src == a) & (Dest == d)
+
